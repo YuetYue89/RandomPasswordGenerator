11c3c640e14deafe992d3d0bb7a11e38048008fd
YuetYue89 <yuetyue0521@gmail.com>
2024-11-22 20:17:50 -0500

    added some more comments

--- p3/passgen/getrandom.c
+++ p3/passgen/getrandom.c
@@ -10,6 +10,8 @@
 // netID: yy655
 // your code for getrandom goes here
 
+// Function that replaces the system's getrandom() func
+// Use "make test" on the CLI in the passgen dir to automatically run this with no LD_PRELOAD and with LD_PRELOAD
 ssize_t getrandom(void *buf, size_t buflen, unsigned int flags) {
 
     // Fill the buffer with non-random password content
