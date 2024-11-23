581cba9c54dee925c99163378cf77221a0a9a7cc
YuetYue89 <yuetyue0521@gmail.com>
2024-11-22 20:07:35 -0500

    finished the first part of the assignment

--- p3/passgen/getrandom.c
+++ p3/passgen/getrandom.c
@@ -1,6 +1,22 @@
+#define _GNU_SOURCE
 #include <sys/random.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <errno.h>
+#include <stddef.h>
 
-// Name:
-// netID:
+
+// Name: Yuet Yue
+// netID: yy655
 // your code for getrandom goes here
+
+ssize_t getrandom(void *buf, size_t buflen, unsigned int flags) {
+
+    // Fill the buffer with non-random password content
+    for (size_t i = 0; i < buflen; i++) {
+        ((unsigned char *)buf)[i] = 0;
+    }
+
+    return buflen;
+}
 
